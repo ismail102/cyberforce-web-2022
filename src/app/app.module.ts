7234b04aea3ad078fe444a225d919f95d689a722
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-10-28 19:35:51 -0500

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
+  },
+];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: AppComponent }]),
+  imports: [BrowserModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
   ],
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
