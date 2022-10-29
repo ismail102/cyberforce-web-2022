c733e927498d21443a024c9878ed7cb2fb5f716c
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-10-28 19:24:09 -0500

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -13,9 +12,9 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([{ path: '', component: HomeComponent }]),
   ],
-  declarations: [AppComponent, ProductListComponent, HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
