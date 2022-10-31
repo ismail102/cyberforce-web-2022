e39ea17cc321429b4c5d1b4446002074845399fb
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-10-30 19:25:19 -0500

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {}
 
   ngOnInit(): void {
+    // this.loginForm = this.fb.group({
+    //   userNameInput: [''],
+    //   passwordInput: [''],
+    // });
   }
 
+  loginSubmit() {
+    // let userName = this.loginFrom.controls.userNameInput.value;
+  }
 }
