25a3128a6bc19c4d0e210f24b905c416d10d27de
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-11-02 16:29:56 -0500

    3 files modified

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,65 +4,141 @@ const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: '10.0.108.76',
+//   user: 'blue108',
+//   password: 'System-Unwary-Random-Canister9',
+//   database: 'solar',
+// });
+
+// connection.connect(function (error) {
+//   if (error) {
+//     throw error;
+//   } else {
+//     console.log('Connected to MySQL! Database name: solar');
+//   }
+// });
+
+// module.exports = connection;
+// // mysql
+// //   .connect('mysql://10.0.108.76:3306/solar')
+// //   .then((x) => {
+// //     console.log(
+// //       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+// //     );
+// //   })
+// //   .catch((err) => {
+// //     console.error('Error connecting to mongo', err.reason);
+// //   });
+
+// const solarRoute = require('./routes/solar.routes');
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+// app.use(cors());
+// // Static directory path
+// app.use(express.static(path.join(__dirname, 'dist')));
+// // API root
+// app.use('/api', solarRoute);
+// // PORT
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//   console.log('Listening on port ' + port);
+// });
+// // 404 Handler
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
+// // Base Route
+// app.get('/', (req, res) => {
+//   res.send('invaild endpoint');
+// });
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
+// // error handler
+// app.use(function (err, req, res, next) {
+//   console.error(err.message);
+//   if (!err.statusCode) err.statusCode = 500;
+//   res.status(err.statusCode).send(err.message);
+// });
+
+console.log('Start...');
+
+var now = new Date();
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/app'));
+
+const conexao = mysql.createConnection({
   host: '10.0.108.76',
   user: 'blue108',
   password: 'System-Unwary-Random-Canister9',
   database: 'solar',
 });
 
-connection.connect(function (error) {
-  if (error) {
-    throw error;
+conexao.connect(function (err) {
+  if (err) {
+    console.log(info() + ' ' + err);
   } else {
-    console.log('Connected to MySQL! Database name: solar');
+    console.log(info() + ' connected...');
   }
 });
 
-module.exports = connection;
-// mysql
-//   .connect('mysql://10.0.108.76:3306/solar')
-//   .then((x) => {
-//     console.log(
-//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error('Error connecting to mongo', err.reason);
-//   });
+function info() {
+  now = new Date();
+  return now.getTime();
+}
 
-const solarRoute = require('./routes/solar.routes');
-const app = express();
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-app.use(cors());
-// Static directory path
-app.use(express.static(path.join(__dirname, 'dist')));
-// API root
-app.use('/api', solarRoute);
-// PORT
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
+app.set('port', process.env.PORT || 4200);
+
+// app.get('/', function(req, res){
+//     console.log(info()+" page request.... ");
+//     res.sendFile(__dirname +'/'+'app/view/index.html');
+// });
+
+app.get('/solar-arr', function (req, res) {
+  console.log(info() + ' clientes request.... ');
+  var sql = 'SELECT * FROM solar_arrays';
+  conexao.query(sql, function (err, result, fields) {
+    if (err) {
+      console.log(info() + ' ' + err);
+      res.send(info() + ': dbErr...');
+    } else {
+      console.log(info() + ' ' + result);
+      res.send(result);
+    }
+  });
 });
-// 404 Handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-// Base Route
-app.get('/', (req, res) => {
-  res.send('invaild endpoint');
-});
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-// error handler
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
+
+// app.post('/clientPost', function(req, res){
+//     var data = req.body;
+//     var dnome = data.nome;
+//     var dmorada = data.morada;
+//     var sql = "INSERT INTO CLIENTES2 (nome,morada) VALUES(?, ?)";
+//     conexao.query(sql, [dnome, dmorada], function(err, result){
+//         if(err){
+//             console.log(info()+":02 "+err);
+//             res.send(info()+": dbErr02...");
+//         }
+//         else
+//         {
+//             console.log(info()+" "+ result);
+//             res.send(result);
+//         }
+//     });
+// });
+
+var dport = app.get('port');
+app.listen(dport, function () {
+  console.log(
+    info() + ' ' + ' app is running at http://10.0.108.79:' + dport + '/'
+  );
+  console.log('   Hit CRTL-C to stop the node server.  ');
 });
--- node-rest-api/routes/solar.routes.js
+++ node-rest-api/routes/solar.routes.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const app = express();
-const solarArrRoute = express.Router();
-let solarArr = require('../model/SolarArr');
+// const express = require('express');
+// const app = express();
+// const solarArrRoute = express.Router();
+// let solarArr = require('../model/SolarArr');
 
-// Get all Book
-solarArrRoute.route('/solar-arr').get((req, res) => {
-  console.log('Res: ', res);
-  solarArr.find((error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.set('Access-Control-Allow-Origin', 'http://10.0.108.79:4200');
-      res.json(data);
-    }
-  });
-});
-// Get Book
-solarArrRoute.route('/solar-arr/:id').get((req, res) => {
-  solarArr.findById(req.params.id, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.set('Access-Control-Allow-Origin', 'http://10.0.108.79:4200');
-      res.json(data);
-    }
-  });
-});
-module.exports = solarArrRoute;
+// // Get all Book
+// solarArrRoute.route('/solar-arr').get((req, res) => {
+//   console.log('Res: ', res);
+//   solarArr.find((error, data) => {
+//     if (error) {
+//       return next(error);
+//     } else {
+//       res.set('Access-Control-Allow-Origin', 'http://10.0.108.79:4200');
+//       res.json(data);
+//     }
+//   });
+// });
+// // Get Book
+// solarArrRoute.route('/solar-arr/:id').get((req, res) => {
+//   solarArr.findById(req.params.id, (error, data) => {
+//     if (error) {
+//       return next(error);
+//     } else {
+//       res.set('Access-Control-Allow-Origin', 'http://10.0.108.79:4200');
+//       res.json(data);
+//     }
+//   });
+// });
+// module.exports = solarArrRoute;
