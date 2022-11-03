80ee6490f7c581d92d3f25b0ea3923f92bfd598d
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-11-03 15:02:41 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: '10.0.108.76',
+  host: 'localhost',
   user: 'blue108',
   password: 'System-Unwary-Random-Canister9',
   database: 'solar',
