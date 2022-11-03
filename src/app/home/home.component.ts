a59c2c9f8918bac5249bba4a9a3b2e8136db649a
ismail102 <42220284+ismail102@users.noreply.github.com>
2022-11-03 15:33:45 -0500

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DataGetService } from '../services/data-get.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  solarArr: any;
 
-  constructor() { }
+  constructor(private dataGetService: DataGetService) {}
 
   ngOnInit(): void {
+    this.dataGetService.getFiles().subscribe((data: any) => {
+      this.solarArr = data;
+      console.log('Data: ', this.solarArr);
+    });
   }
-
 }
